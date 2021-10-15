bfaf6a2a8c367be35a1184900dd64f7b2fe2fd40
Renan Corredato <renancorredato@hotmail.com>
2021-10-15 18:19:44 -0300

    Funcao recuperando valores digitados

--- app/src/main/java/com/masterclass/calculanotas/MainActivity.kt
+++ app/src/main/java/com/masterclass/calculanotas/MainActivity.kt
@@ -2,10 +2,53 @@ package com.masterclass.calculanotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.masterclass.calculanotas.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.round
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btCalculete.setOnClickListener {
+            btCalculateNote()
+
+        }
     }
+
+    private fun btCalculateNote() {
+        val note1 = amount_input1.text.toString()
+        val note2 = amount_input2.text.toString()
+        val note3 = amount_input3.text.toString()
+        caculateNote(note1,note2,note3)
+
+
+    }
+
+    private fun caculateNote(ac1: String, ac2: String, ac3: String) {
+        val validateAc1 = ac1.toDouble()
+        val validateAc2 = ac2.toDouble()
+        val validateAc3 = ac3.toDouble()
+
+        val resulValidadeNote = validateAc1 + validateAc2 + validateAc3
+        val result:Double = resulValidadeNote / 3
+        val rounded = String.format("%.2f", result) // rounds to 3 decimal places
+
+
+        if (rounded >= 5.toString()){
+            tvResult.text = "Aprovado$rounded"
+        }else{
+            tvResult.text = "Reprovado$rounded"
+        }
+
+    }
+
 }
